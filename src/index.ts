8f4dde787cf111a438df93cf049b1400ac37d045
Ronan Limon Duparcmeur <ronan.duparcmeur@lifen.fr>
2021-09-05 13:54:13 +0200

    Handle css classes for copied state
    In addition to changing the target button's text, the Stimulus class `copied`
    can be used to give a visual feedback when copying to the clipboard.
    
    If defined, the `copied` class is added when the content is copied, and removed
    when the initial text is restored.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,21 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   hasButtonTarget: boolean
+  hasCopiedClass: boolean
   originalText: string
   successDuration: number
   successDurationValue: number
   timeout: number
   buttonTarget: HTMLElement
   sourceTarget: HTMLInputElement
+  copiedClass: string
 
   static targets = ['button', 'source']
   static values = {
     successDuration: Number
   }
+
+  static classes = ['copied']
 
   connect (): void {
     if (!this.hasButtonTarget) return
@@ -38,9 +42,15 @@ export default class extends Controller {
     }
 
     this.buttonTarget.innerText = this.data.get('successContent')
+    if (this.hasCopiedClass) {
+      this.buttonTarget.classList.add(this.copiedClass)
+    }
 
     this.timeout = setTimeout(() => {
       this.buttonTarget.innerText = this.originalText
+      if (this.hasCopiedClass) {
+        this.buttonTarget.classList.remove(this.copiedClass)
+      }
     }, this.successDuration)
   }
 }
