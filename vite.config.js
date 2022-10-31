46e1ee06ffc27026c8da331c201e3d9aaa140806
Guillaume Briday <guillaumebriday@gmail.com>
2022-10-31 14:32:30 +0100

    Config and bump dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-const path = require('path')
+import path from 'path'
 
-module.exports = {
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'stimulus-clipboard'
-    },
-    rollupOptions: {
-      external: ['@hotwired/stimulus'],
-      output: {
-        globals: {
-          '@hotwired/stimulus': 'Stimulus'
+export default ({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
+
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'stimulus-clipboard'
+      },
+      rollupOptions: {
+        external: ['@hotwired/stimulus'],
+        output: {
+          globals: {
+            '@hotwired/stimulus': 'Stimulus'
+          }
         }
       }
     }
